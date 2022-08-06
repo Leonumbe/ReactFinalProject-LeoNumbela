c2238340149f2f512ab4a6ee903001e4f9596800
LeoNumbela <leonumbela@gmail.com>
2022-08-06 14:37:37 -0300

    se prepara desafio 05

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -12,10 +12,32 @@ function Card({ name, productType, price, rating, image, description }) {
           <h3>$ {price}</h3>
           <h3>Value: {rating}</h3>
           <h3> {description}</h3>
-          <ItemCount/>
+          <ItemCount minStock={1} maxStock={5}/>
         </div>
       </div>
     );
   }
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function BasicExample() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default BasicExample;
   
-  export default Card;
+//export default Card;
+  
