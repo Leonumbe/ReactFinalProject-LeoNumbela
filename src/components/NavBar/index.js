48b64b6ebf7be28807684acba66bde9cfbc826e5
LeoNumbela <leonumbela@gmail.com>
2022-08-08 22:14:05 -0300

    se ajusta header, se hace responsive la navbar, se agregra boton mas input para buscar categoria sin funcionabilidad aun, se busca un diseno acorde a un e-commerce, resta definir paleta de colores, mejorar footer

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,24 +2,71 @@ import "./main.css";
 import CartWidget from "./CartWidget/index";
 import { Link } from "react-router-dom";
 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
 function NavBar(){
     return(
         <header>
             <nav id="" className="NavBar">
                 <div className="max-width">
                     <div className="containerNavbar">
+                        <div className="contNavB">
                         <div className="contLogo">
                             <img className="logo" src="/LogoAdap.png" alt='bakers team logo'/>
                         </div>
-                        <ul className="menu">
-                            <li><Link to="/" className="">Home</Link></li>
-                            <li><Link to="/" className="">About Us</Link></li>
-                            <li><Link to="/" className="">Products</Link></li>
-                            <li><Link to="/" className="">Faqs</Link></li>
-                            <li><Link to="/" className="">Contact Us</Link></li>
-                        </ul>
-                        <CartWidget/>
+                        <Navbar  expand="md">
+                         <Container fluid>
+                         <Navbar.Toggle aria-controls="navbarScroll" />
+                         <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="me-auto my-2 my-lg-0"
+                                style={{ maxHeight: '100px' }}
+                                navbarScroll
+                            >
+                                <Nav.Link href="#action1">About Us</Nav.Link>
+                                <Nav.Link href="#action2">Products</Nav.Link>
+                                <Nav.Link href="#action2">Faqs</Nav.Link>
+                                <Nav.Link href="#action2">Contact Us</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+
                     </div>
+
+                       <div>
+                        <CartWidget/>
+                       </div>
+                    </div>
+                    <div>
+                    
+                    </div>
+                   
+                    <div>
+                   
+                            <Form className="d-flex">
+                                <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                                />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+  
+
+
+
+                    </div>
+
+
+
                 </div>
             </nav>
         </header>
