b08150bd65b6a756957e279a5b18d3c0dc0588f6
LeoNumbela <leonumbela@gmail.com>
2022-08-14 11:36:58 -0300

    se modifican a href, las mismas estaban renderizando el footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,26 +1,22 @@
 import "./main.css";
 import {Link} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons' ;
+import { faLinkedin, faInstagram  } from '@fortawesome/free-brands-svg-icons' ;
 import { faCopyright } from '@fortawesome/free-solid-svg-icons' ;
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 
-
-function Footer(){
+export default function Footer(){
     return(
         <footer>
             <span>Create by <Link to="/">LeoN.</Link> | <span><FontAwesomeIcon icon={faCopyright} /></span> 2022 All right reserved.</span>
             <div className="social">
-                <a href="http://www.linkedin.com/in/leonardo-numbela-63942254" target="blank">
+                <Link to="http://www.linkedin.com/in/leonardo-numbela-63942254" target="blank">
                     <FontAwesomeIcon className="i" icon={faLinkedin} /> 
-                </a>
-                <a href="https://www.instagram.com/leonumbela/" target="blank">
+                </Link>
+                <Link to="https://www.instagram.com/leonumbela/" target="blank">
                     <FontAwesomeIcon className="i" icon={faInstagram}/>
-                </a>
+                </Link>
             </div>
         </footer > 
     )
 }
-
-export default Footer;
