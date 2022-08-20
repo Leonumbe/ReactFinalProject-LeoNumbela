1cf385ffe19e4dd4ef6503602c6bea52ec389ee4
LeoNumbela <leonumbela@gmail.com>
2022-08-20 16:24:58 -0300

    ajuste

--- src/components/NavBar/CartWidget/index.jsx
+++ src/components/NavBar/CartWidget/index.jsx
@@ -3,11 +3,16 @@ import {FontAwesomeIcon} from"@fortawesome/react-fontawesome";
 import {faShoppingCart} from"@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
+import React, {useContext} from "react";
+import { cartContext } from "../../../context/cartContext";
+
 function CartWidget() {
+    const {cart} = useContext(cartContext)
     return(
         <div className="cart">
             <Link to="/cart"><FontAwesomeIcon className="cartSet" icon={faShoppingCart}/></Link>
-        </div>
+            <span>{cart.quantity}</span>
+            </div>
     )
 }
 
