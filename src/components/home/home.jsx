430282b1795bba9313c2b7c4f05338e9dec0455f
LeoNumbela <leonumbela@gmail.com>
2022-09-08 13:05:06 -0300

    se ajusta home con map

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,22 +1,34 @@
 import "./main.css"
 
-
-
 export default function Home(){
+    
+    const brandMessage =[
+        {textA: "We don't have all the models..."},
+        {textB:"We only have the best!"},
+        {textC:"Baker.", textC2:"team"}
+    ]
     return(
         <div className="home" >
         <div className="max-width">
             <div className="homeContent">
-            <div className="homeImg">
-                <img className="imgHome" src="https://i.ibb.co/Y2GR47B/frente-Nike.jpg" alt="frente-Nike" border="0" />
-            </div>
-            <div className="homePhrases">
-                <h1 className="homeText">We don't have all the models...</h1>
-                <h1 className="homeText">We only have the best!</h1>
-                <h1 className="homeText">Baker. <spam className="homeTextSpam"> team</spam></h1>
-            </div>
-        </div>
-        </div>
+                        <div className="homeImg">
+                                    <img className="imgHome" src="https://i.ibb.co/Y2GR47B/frente-Nike.jpg" alt="frente-Nike"/>
+                                </div>
+                                <div className="homePhrases">
+                {brandMessage.map((item)=>{
+                    return(
+
+                        <>
+                                    <h1 className="homeText">{item.textA}</h1>
+                                    <h1 className="homeText">{item.textB}</h1>
+                                    <h1 className="homeText">{item.textC}<spam className="homeTextSpam">{item.textC2}</spam></h1>
+                        </>
+                        )
+                    }
+                    )}
+                    </div>
+                    </div>
+                </div>
         </div>
     )
 }
